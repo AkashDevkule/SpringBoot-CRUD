cc3ab3f558a3ebc1cc55861ba2c33cb348172533
AkashDevkule <skydevkule99@gmail.com>
2022-06-27 18:09:59 +0530

    repository interface updated

--- src/main/java/com/employee/repository/EmpRepository.java
+++ src/main/java/com/employee/repository/EmpRepository.java
@@ -1,9 +1,13 @@
 package com.employee.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.employee.entities.Emp;
 
 public interface EmpRepository extends CrudRepository<Emp, Integer> {
 
+	public Optional<Emp> findByEmpEmail(String email);
+	public Optional<Emp> findByEmpMobNo(Long mobno);
 }
